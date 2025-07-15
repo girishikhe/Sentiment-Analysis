77c683a9fd79581753b744ec35b390bfa48d0412
Girish <girish.ikhe.vu@gmail.com>
2025-07-15 19:43:25 +0530

    The environment is successfully installed

---
+++ template.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from pathlib import Path 
+
+project_name="sentiment_analysis"
+
+list_files=[
+    list_of_files = [
+
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/register.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "params.yaml"
+]
+]
