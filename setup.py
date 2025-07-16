a9286da9ea9f313ae5e68e45acab3967e8f9dd61
Girish <girish.ikhe.vu@gmail.com>
2025-07-16 09:39:57 +0530

    Folder Structure is updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'sentiment_analysis',
+    version= '0.0.0',
+    author= 'Girish',
+    author_email= 'girish.ikhe.vu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
